e882870865d38b38a2a1e44b00b607bb6970e3bf
mikezks <23002661+mikezks@users.noreply.github.com>
2023-11-16 15:43:12 +0100

    feat: error handling, implicit unsubscribe

--- libs/tickets/feature-booking/src/lib/flight-typeahead/flight-typeahead.component.ts
+++ libs/tickets/feature-booking/src/lib/flight-typeahead/flight-typeahead.component.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { Flight, FlightService } from '@flight-demo/tickets/domain';
-import { Observable, debounceTime, distinctUntilChanged, filter, switchMap, tap } from 'rxjs';
+import { Observable, Subject, catchError, debounceTime, distinctUntilChanged, filter, of, switchMap, takeUntil, tap, timer } from 'rxjs';
 
 @Component({
   selector: 'tickets-flight-typeahead',
@@ -14,12 +15,22 @@ import { Observable, debounceTime, distinctUntilChanged, filter, switchMap, tap 
   templateUrl: './flight-typeahead.component.html',
   styleUrls: ['./flight-typeahead.component.css'],
 })
-export class FlightTypeaheadComponent {
+export class FlightTypeaheadComponent /* implements OnDestroy  */{
   private flightService = inject(FlightService);
 
   protected control = new FormControl('', { nonNullable: true });
   protected flights$ = this.initFlightsStream();
   protected loading = false;
+
+  private destroy$ = new Subject<void>();
+  private takeUntilDestroyed = takeUntilDestroyed;
+
+  constructor() {
+    timer(0, 1_000).pipe(
+      // takeUntil(this.destroy$),
+      this.takeUntilDestroyed()
+    ).subscribe(console.log);
+  }
 
   initFlightsStream(): Observable<Flight[]> {
     /**
@@ -39,9 +50,16 @@ export class FlightTypeaheadComponent {
        * Stream 2: Flight Data Access Service - Backend API Call: Array of Flights
        *  - State Provider: Final View State
        */
-      switchMap(city => this.flightService.find(city, '')),
+      switchMap(city => this.flightService.find(city, '').pipe(
+        // Error Handling: Fallback Stream - Empty Array
+        catchError(() => of([]))
+      )),
       // Side-Effect: Set Loading Prop
       tap(() => this.loading = false)
     );
   }
+
+  /* ngOnDestroy(): void {
+    this.destroy$.next();
+  } */
 }
