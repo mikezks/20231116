f8a2a4367a7cd3543ea289fa629ed6926223dcda
mikezks <23002661+mikezks@users.noreply.github.com>
2023-11-17 15:38:15 +0100

    feat: signals demo

--- libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
+++ libs/tickets/feature-booking/src/lib/flight-search/flight-search.component.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { Component, inject } from '@angular/core';
+import { Component, computed, effect, inject, signal } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CityPipe } from '@flight-demo/shared/ui-common';
 import { ticketsActions, ticketsFeature } from '@flight-demo/tickets/domain';
 import { Store } from '@ngrx/store';
 import { FlightCardComponent } from '../flight-card/flight-card.component';
+import { toObservable, toSignal } from '@angular/core/rxjs-interop';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-flight-search',
@@ -16,24 +18,39 @@ import { FlightCardComponent } from '../flight-card/flight-card.component';
 export class FlightSearchComponent {
   private store = inject(Store);
 
-  from = 'London';
-  to = 'New York';
-  protected flights$ = this.store.select(ticketsFeature.selectFlights);
+  from = signal('London');
+  to = signal('New York');
+  lazyFrom$ = toObservable(this.from).pipe(
+    debounceTime(1_000)
+  );
+  lazyFrom = toSignal(this.lazyFrom$, {
+    initialValue: this.from()
+  });
+  flightRoute = computed(
+    () => 'From ' + this.lazyFrom() + ' to ' + this.to() + '.'
+  );
+  protected flights = this.store.selectSignal(ticketsFeature.selectFlights);
 
   basket: Record<number, boolean> = {
     3: true,
     5: true,
   };
 
+  constructor() {
+    effect(
+      () => console.log(this.flightRoute())
+    );
+  }
+
   search(): void {
-    if (!this.from || !this.to) {
+    if (!this.from() || !this.to()) {
       return;
     }
 
     this.store.dispatch(
       ticketsActions.flightsLoad({
-        from: this.from,
-        to: this.to
+        from: this.from(),
+        to: this.to()
       })
     );
   }
